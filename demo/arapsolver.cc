f1e990473589b73d5885f3821c96c95d9619aafe
dut09 <dutao09@gmail.com>
2014-09-06 11:29:02 -0700

    Add Get functions, cot_weight data member and comments in ArapSolver.

--- demo/arapsolver.cc
+++ demo/arapsolver.cc
@@ -23,11 +23,42 @@ bool ArapSolver::RegisterData(const Eigen::MatrixXd& vertices,
 }
 
 void ArapSolver::Precompute() {
-
+  // Compute cot_weight.
 }
 
 void ArapSolver::Solve(const Eigen::MatrixXd& fixed_vertices) {
 
+}
+
+Eigen::Vector3d ArapSolver::ComputeCotangent(int face_id) const {
+  Eigen::Vector3d cotangent(0.0, 0.0, 0.0);
+  // The triangle is defined as follows:
+  //            A
+  //           /  -
+  //        c /    - b
+  //         /        -
+  //        /    a      -
+  //       B--------------C
+  // where A, B, C corresponds to faces_(face_id, 0), faces_(face_id, 1) and
+  // faces_(face_id, 2). The return value is (cotA, cotB, cotC).
+  // Compute the triangle area first.
+  Eigen::Vector3d A = vertices_.row(faces_(face_id, 0));
+  Eigen::Vector3d B = vertices_.row(faces_(face_id, 1));
+  Eigen::Vector3d C = vertices_.row(faces_(face_id, 2));
+  double a_squared = (B - C).squaredNorm();
+  double b_squared = (C - A).squaredNorm();
+  double c_squared = (A - B).squaredNorm();
+  // Compute the area of the triangle. area = 1/2bcsinA.
+  double area = (B - A).cross(C - A).norm() / 2;
+  // Compute cotA = cosA / sinA.
+  // b^2 + c^2 -2bccosA = a^2, or cosA = (b^2 + c^2 - a^2) / 2bc.
+  // 1/2bcsinA = area, or sinA = 2area/bc.
+  // cotA = (b^2 + c^2 -a^2) / 4area.
+  double four_area = 4 * area;
+  cotangent(0) = (b_squared + c_squared - a_squared) / four_area;
+  cotangent(1) = (c_squared + a_squared - b_squared) / four_area;
+  cotangent(2) = (a_squared + b_squared - c_squared) / four_area;
+  return cotangent;
 }
 
 }  // namespace demo
--- demo/arapsolver.h
+++ demo/arapsolver.h
@@ -11,33 +11,52 @@ class ArapSolver {
  public:
   ArapSolver();
 
-  // Register data for ArapSolver. Once vertices and faces are set then cannot
+  // Registers data for ArapSolver. Once vertices and faces are set then cannot
   // be reset from outside. The only way to reset vertices is to call Solve,
   // which will run ARAP algorithm based on the current state, and update
   // vertices_.
   bool RegisterData(const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
       const Eigen::VectorXi& selected, int max_iteration);
 
-  // Pre compute cotangent weight and left matrix in ARAP.
+  // Pre computes cotangent weight and left matrix in ARAP.
   void Precompute();
 
-  // Solve ARAP problem. |fixed_vertices| is a # of fixed vertices by 3 matrix.
+  // Solves ARAP problem. |fixed_vertices| is a # of fixed vertices by 3 matrix.
   // Each row in |fixed_vertices| represents the coordinates of a fixed vertex.
   void Solve(const Eigen::MatrixXd& fixed_vertices);
 
-  // Get vertices_.
+  // Gets vertices_.
   const Eigen::MatrixXd& GetVertices() const { return vertices_; }
 
-  // Get faces_.
+  // Gets faces_.
   const Eigen::MatrixXi& GetFaces() const { return faces_; }
 
-  // Get indices of the selected vertices.
+  // Gets indices of the selected vertices.
   const Eigen::VectorXi& GetSelectedIndices() const { return selected_; }
 
  private:
+  // Computes the cotangent angle in one face indicated by |face_id|.
+  Eigen::Vector3d ComputeCotangent(int face_id) const;
+
+  // vertices_ is a # of vertices by 3 matrix. Each row in vertices_ represents
+  // a vertex's position.
   Eigen::MatrixXd vertices_;
+  // faces_ is a # of faces by 3 matrix. Each row contains the indices of this
+  // face's three vertices.
   Eigen::MatrixXi faces_;
+  // selected_ is a vector no longer than the # of vertices. It contains the
+  // indices of the vertices that we want to fix during the deformation.
   Eigen::VectorXi selected_;
+
+  // This is a sparse matrix used to store the cotangent weight. The # of rows
+  // and columns equals to the # of vertices, i.e., vertices_.rows(). For any i
+  // and j, the definition of cot_weight(i, j) is as follows:
+  // If i != j and (i, j) is not an edge, cot_weight(i, j) = 0.
+  // If i != j and (i, j) is an edge, then cot_weight(i, j) equals to:
+  //   cot_weight(i, j) = 1/2(cot alpha_ij + cot beta_ij).
+  // Note that cot_weight(i, j) = cot_weight(j, i), or cot_weight is symmetric.
+  // If i == j, then cot_weight(i, i) is defined as the sum of all the w_ij.
+  Eigen::SparseMatrix<double> cot_weight;
 
   // Max number of iterations used to solve ARAP.
   int max_iteration_;
