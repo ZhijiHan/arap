6bdc9436058dc4cb0c4303738eb2f9d6bcd58852
dut09 <dutao09@gmail.com>
2014-10-09 13:34:19 -0700

    Build a light thread only for displaying OFF models.

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Change files below for different models.
 OFF_FILE_NAME=/home/taodu/research/arap/model/decimated-knight.off
-DMAT_FILE_NAME=/home/taodu/research/arap/model/decimated-knight-selection.dmat
 
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
-./demo_bin $OFF_FILE_NAME $DMAT_FILE_NAME
+./demo_bin $OFF_FILE_NAME
