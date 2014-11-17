e12cffa70116442e3a6cdacd5259bf51f38e515d
dut09 <dutao09@gmail.com>
2014-11-17 11:22:34 -0800

    Add support for displaying constraints.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Change files below for different models.
 OFF_FILE_NAME=/home/taodu/research/arap/model/decimated-knight.off
+DMAT_FILE_NAME=/home/taodu/research/arap/model/decimated-knight.dmat
 
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
-./demo_bin $OFF_FILE_NAME
+./demo_bin $OFF_FILE_NAME $DMAT_FILE_NAME
--- demo/main.cc
+++ demo/main.cc
@@ -1,14 +1,28 @@
 #include <igl/readOFF.h>
+#include <igl/readDMAT.h>
 #include <igl/viewer/Viewer.h>
 
-Eigen::MatrixXd V;
-Eigen::MatrixXi F;
+static const Eigen::RowVector3d kPurple(80.0 / 255.0,
+                                        64.0 / 255.0,
+                                        255.0 / 255.0);
 
 int main(int argc, char* argv[]) {
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  Eigen::MatrixXd bc;
+
   igl::readOFF(argv[1], V, F);
+  igl::readDMAT(argv[2], bc);
 
   igl::Viewer viewer;
   viewer.data.set_mesh(V, F);
+
+  // Set colors for selected vertices.
+  Eigen::MatrixXd C(bc.rows(), 3);
+  for (int i = 0; i < bc.rows(); ++i) {
+    C.row(i) = kPurple;
+  }
+  viewer.data.set_points(bc, C);
   viewer.launch();
   return 0;
 }
