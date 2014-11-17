76743ef25933fa272c06cc2a3d1ead067fef1f05
dut09 <dutao09@gmail.com>
2014-11-17 12:29:20 -0800

    Add options to display constraints in display branch.

--- demo/main.cc
+++ demo/main.cc
@@ -2,27 +2,29 @@
 #include <igl/readDMAT.h>
 #include <igl/viewer/Viewer.h>
 
+// Color for the final destination of constrained vertices.
 static const Eigen::RowVector3d kPurple(80.0 / 255.0,
                                         64.0 / 255.0,
                                         255.0 / 255.0);
 
+// Displays vertices, faces and possibly vertex constraints.
+// Usage: .OFF file [.DMAT file]
 int main(int argc, char* argv[]) {
   Eigen::MatrixXd V;
   Eigen::MatrixXi F;
-  Eigen::MatrixXd bc;
 
+  // Display the object itself.
   igl::readOFF(argv[1], V, F);
-  igl::readDMAT(argv[2], bc);
-
   igl::Viewer viewer;
   viewer.data.set_mesh(V, F);
 
-  // Set colors for selected vertices.
-  Eigen::MatrixXd C(bc.rows(), 3);
-  for (int i = 0; i < bc.rows(); ++i) {
-    C.row(i) = kPurple;
+  if (argc > 2) {
+    Eigen::MatrixXd bc;
+    igl::readDMAT(argv[2], bc);
+    // Display the final destination.
+    Eigen::MatrixXd C = Eigen::VectorXd::Ones(bc.rows()) * kPurple;
+    viewer.data.set_points(bc, C);
   }
-  viewer.data.set_points(bc, C);
   viewer.launch();
   return 0;
 }
